567642da7d5c105f01450be3e0785c69b63e0c85
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-15 22:35:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,5 +5,21 @@
 
 
 def key_for_min_value(name_hash)
- name_hash.each do 
+ name_a = name_hash.to_a
+
+  # Default key value
+  d_value= 1000
+  d_key= 0
+
+  # Iterate new array 
+  name_a.each do |i|
+      # If current value is lower than default, change value&key 
+      if i[1] < d_value
+        d_value = i[1]
+        d_key = i[0]
+      end
+
+   end
+
+  return d_key
 end
